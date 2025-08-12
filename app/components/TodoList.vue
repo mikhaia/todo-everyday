c4a14216636628dd0aa3c2b9dc1721c08198547f
Alexandr Mikhailov <alexander@enclave.vn>
2025-08-12 18:20:41 +0700

    Change color and title by category

--- app/components/TodoList.vue
+++ app/components/TodoList.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="max-w-3xl">
-    <h2 class="text-2xl font-bold mb-4 flex items-center gap-1">
+    <h2 class="text-2xl font-bold mb-4 flex items-center gap-1"
+      :style="{ color: activeCategory?.background? textColor(activeCategory?.background) : '' }">
       <span
-        class="material-symbols-outlined"
+        class="material-symbols-outlined text-4xl"
         v-if="activeCategory?.icon"
         >{{ activeCategory.icon }}</span
       >
@@ -187,5 +188,23 @@ const toggle = async (i: number) => {
   if (t?.id) await updateDoc(doc(db, 'users', user.value.uid, 'todos', t.id), {
     done: !t.done
   })
+}
+
+function textColor(bg: string) {
+  const { r, g, b } = toRGB(bg);
+  const L = (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
+  return L > 0.6 ? '#111827' : '#ffffff';
+}
+
+function toRGB(color: string) {
+  if (!color) return { r: 0, g: 0, b: 0 };
+  if (color.startsWith('#')) {
+    const hex = color.slice(1).replace(/^(.)(.)(.)$/, '$1$1$2$2$3$3');
+    const num = parseInt(hex, 16);
+    return { r: (num >> 16) & 255, g: (num >> 8) & 255, b: num & 255 };
+  }
+
+  const m = color.match(/\d+/g);
+  return m ? { r: +m[0], g: +Number(m[1]), b: +Number(m[2]) } : { r: 0, g: 0, b: 0 };
 }
 </script>
