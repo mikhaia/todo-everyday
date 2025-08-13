b177ce3da5fb90359c117abe408b1cab2997bbf0
Alexandr Mikhailov <alexander@enclave.vn>
2025-08-13 17:39:10 +0700

    Implement background for categories. Add Firebase Storage.

--- app/components/TodoList.vue
+++ app/components/TodoList.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="max-w-3xl text-black">
     <h2 class="text-2xl font-bold mb-4 flex items-center gap-1"
-      :style="{ color: activeCategory?.background? textColor(activeCategory?.background) : '' }">
+      :style="{
+        color: activeCategory?.image
+          ? '#fff'
+          : (activeCategory?.background ? textColor(activeCategory.background) : undefined)
+      }">
       <span
         class="material-symbols-outlined text-4xl"
         v-if="activeCategory?.icon"
