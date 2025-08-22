fbea9767c750b47228fc1437dade3f20948d926b
Alexandr Mikhailov <alexander@enclave.vn>
2025-08-23 00:13:33 +0700

    Add login page

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,10 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
-  const user = useState<{uid:string}|null>('user')
+  const user = useState<{ uid: string } | null>('user')
+
   if (to.path.startsWith('/share')) return
-  if (!user.value && to.path !== '/login') {
-    return navigateTo('/login')
-  }
-  if (user.value && to.path === '/login') {
-    return navigateTo('/')
-  }
+  if (!user.value && to.path !== '/login') return navigateTo('/login')
+  if (user.value && to.path === '/login') return navigateTo('/')
 })
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-    <div class="w-full max-w-md bg-gray-800 rounded-lg shadow-lg p-8 animate-pulse-glow flex">
-      <div class="relative hidden md:block w-1/2 pr-6">
+  <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white bg-[url('assets/img/back.jpg')] bg-cover bg-center">
+    <div class="w-full max-w-xl bg-gray-800 rounded-lg flex shadow-[0_0_20px_rgba(0,0,0,1)]">
+      <div class="relative hidden md:block w-1/2">
         <img
-          src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=300&q=60"
+          src="assets/img/login.jpg"
           alt="Background"
           class="object-cover w-full h-full rounded"
         />
         <span
-          class="material-symbols-outlined absolute inset-0 flex items-center justify-center text-white text-6xl"
+          class="material-symbols-outlined absolute inset-0 flex items-start justify-start text-white text-6xl m-3"
         >early_on</span>
       </div>
-      <div class="flex-1">
+      <div class="flex-1 p-6">
         <h2 class="text-2xl font-bold mb-6 text-center">Sign in</h2>
         <form class="space-y-4" @submit.prevent="loginWithEmail">
           <input v-model="email" type="email" placeholder="Email" class="w-full p-2 rounded bg-gray-700 placeholder-gray-400" />
           <input v-model="password" type="password" placeholder="Password" class="w-full p-2 rounded bg-gray-700 placeholder-gray-400" />
           <button type="submit" class="w-full py-2 bg-blue-500 rounded">Sign in</button>
         </form>
-        <div class="mt-4 flex flex-col gap-2">
-          <button class="w-full py-2 bg-red-600 rounded flex items-center justify-center gap-2" @click="loginGoogle">
-            <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" class="w-5 h-5" />
+        <div class="mt-4 flex flex-col gap-4 text-black">
+          <button class="w-full py-2 bg-gray-50 rounded flex items-center justify-center gap-2" @click="loginGoogle">
+            <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" class="w-7 h-7" />
             Sign in with Google
           </button>
-          <button class="w-full py-2 bg-gray-700 rounded flex items-center justify-center gap-2" @click="loginGithub">
-            <img src="https://www.svgrepo.com/show/475661/github-color.svg" alt="Github" class="w-5 h-5" />
+          <button class="w-full py-2 bg-gray-50 rounded flex items-center justify-center gap-2" @click="loginGithub">
+            <img src="https://www.svgrepo.com/download/512317/github-142.svg" alt="Github" class="w-7 h-7" />
             Sign in with Github
           </button>
         </div>
